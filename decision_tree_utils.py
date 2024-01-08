6bf1fc0ed0a43c55e5886176be0b0e93d4b22f43
Foca Bogdan <81586357+Zeyt8@users.noreply.github.com>
2024-01-08 17:08:10 +0200

    Finish MyDecisionTree

--- decision_tree_utils.py
+++ decision_tree_utils.py
@@ -1,7 +1,8 @@
 from collections import Counter
-from copy import deepcopy
 from math import log2
+import pandas as pd
 import numpy as np
+import numpy.typing as npt
 
 class Node:
     """ Representation for a node from the decision tree """
@@ -11,69 +12,66 @@ class Node:
             for leafs it is the class
         """
         self.label = label
-        
         # Dictionary of (attribute value, nodes)
         self.children = {}
-    
-    def display(self, indent = ""):
-        print(indent + (self.label + ":" if self.children else "<" + self.label + ">"))
-        indent += "   "
-        if self.children:
-            for key, value in self.children.items():
-                print(indent + ":" + key)
-                value.display(indent + "   ")
 
 class MyDecisionTreeClassifier:
 
-    def __init__(self, df, T):
-        self.attributes = [a for a in df.columns if a != "Revenue"]
-        self.classes = np.unique(T)
+    def __init__(self, df, Class):
+        self.df = df
+        self.Class = Class
 
-    def fit(self, X, T):
-        self.tree = self.__id3(X, T, self.attributes, 3)
+    def fit(self, X, T) -> None:
+        self.tree = self.__id3(self.df, self.df.drop(self.Class, axis=1).columns, 3)
 
-    def evaluate(self, example):
-        if self.tree.children == {}:
-            return self.tree.label
-        else:
-            attr = self.tree.label
-            val = example[attr]
-            if val not in self.tree.children:
-                return None
-            return self.evaluate(self.tree.children[val], example)
+    def predict(self, X) -> npt.NDArray[np.bool_]:
+        results = np.empty(len(X), dtype=np.bool_)
 
-    def __id3(self, X, T, A, d = 6):
-        if len(set([t for t in T])) == 1:
-            return Node(T[0])
+        for i, example in enumerate(X):
+            results[i] = self.__evaluate(self.tree, example)
+
+        return results
+
+    def __id3(self, df: pd.DataFrame, A: pd.Index, d = 6) -> Node:
+        if len(df[self.Class].unique()) == 1:
+            return Node(df.iloc[0][self.Class])
         if len(A) == 0 or d == 0:
-            return Node(self.__mostFrequentClass(T))
-        A_ = deepcopy(A)
-        a_star = max(A_, key=lambda a: self.__gain(X, T, a))
-        A_.remove(a_star)
+            return Node(self.__mostFrequentClass(df))
+        A_copy = A.copy()
+        a_star = max(A_copy, key=lambda a: self.__gain(df, a))
+        A_copy.drop(a_star)
         n = Node(a_star)
-        for val in set([x[a_star] for x in X]):
-            X_val = [x for x in X if x[a_star] == val]
-            T_val = [t for t, x in zip(T, X) if x[a_star] == val]
-            n.children[val] = self.__id3(X_val, T_val, A_, d - 1)
+        for val in df[a_star].unique():
+            df_val = df[df[a_star] == val]
+            n.children[val] = self.__id3(df_val, A_copy, d - 1)
         return n
 
-    def __mostFrequentClass(self, T):
-        c: Counter = Counter()
-        for t in T:
-            c.update([t])
+    def __mostFrequentClass(self, df: pd.DataFrame) -> str:
+        c: Counter = Counter(df[self.Class])
         return c.most_common(1)[0][0]
 
-    def __entropy(self, T):
-        entropy  = 0
-        for c in self.classes:
-            p = len([t for t in T if t == c]) / len(T)
+    def __entropy(self, df: pd.DataFrame) -> float:
+        entropy = 0
+        total_records = len(df)
+        for c in df[self.Class].unique():
+            p = len(df[df[self.Class] == c]) / total_records
             if p != 0:
                 entropy += -p * log2(p)
         return entropy
 
-    def __gain(self, X, T, a):
-        gain = self.__entropy(T)
-        for val in set([x[a] for x in X]):
-            X_val = [x for x in X if x[a] == val]
-            gain -= len(X_val) / len(X) * self.__entropy(X_val)
+    def __gain(self, df: pd.DataFrame, a) -> float:
+        gain = self.__entropy(df)
+        total_records = len(df)
+        for val in df[a].unique():
+            df_val = df[df[a] == val]
+            gain -= len(df_val) / total_records * self.__entropy(df_val)
         return gain
+    
+    def __evaluate(self, tree, example):
+        if tree.children == {}:
+            return bool(tree.label)
+        else:
+            attr = tree.label
+            val = example[self.df.columns.get_loc(attr)]
+            closest_child = min(tree.children.keys(), key=lambda x: abs(x - val))
+            return self.__evaluate(tree.children[closest_child], example)
--- logistic.py
+++ logistic.py
@@ -1,8 +1,9 @@
 import numpy as np
+import numpy.typing as npt
 
 class MyLogisticRegression:
 
-    def fit(self, X, T, lr=.01, epochs_no=100):
+    def fit(self, X: npt.NDArray[np.float32], T: npt.NDArray[np.float32], lr: float = .01, epochs_no: int = 100) -> None:
         (N, D) = X.shape
         X_hat = np.concatenate([X, np.ones((N, 1))], axis=1)
         self.W = np.random.randn((D+1))
@@ -11,11 +12,11 @@ class MyLogisticRegression:
             Y = self.__logistic(np.dot(X_hat, self.W))
             self.W -= lr * np.dot(X_hat.T, Y - T) / N
     
-    def predict(self, X):
+    def predict(self, X: npt.NDArray[np.float32]) -> npt.NDArray[np.bool_]:
         (N, _) = X.shape
         X_hat = np.concatenate([X, np.ones((N, 1))], axis=1)
         Y = self.__logistic(np.dot(X_hat, self.W))
         return Y > .5
     
-    def __logistic(self, x):
+    def __logistic(self, x: float) -> float:
         return 1. / (1. + np.exp(-x))
