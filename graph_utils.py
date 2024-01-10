0c15818a1683e9b9b32c1c8000d1d9605dbaf9bb
Foca Bogdan <81586357+Zeyt8@users.noreply.github.com>
2024-01-10 16:12:33 +0200

    Fix correlation graphs

--- graph_utils.py
+++ graph_utils.py
@@ -11,17 +11,16 @@ def show_frequency(df: pd.DataFrame, column_name: str) -> None:
     plt.ylabel('Frequency')
     plt.show()
 
-def show_numerical_attribute_distribution(df: pd.DataFrame) -> None:
-    numerical_attributes = df.columns
-    for attribute in numerical_attributes:
+def show_numerical_attribute_distribution(numerical_attributes: pd.DataFrame) -> None:
+    for attribute in numerical_attributes.columns:
         counts = []
-        min_value = df[attribute].min()
-        max_value = df[attribute].max()
+        min_value = numerical_attributes[attribute].min()
+        max_value = numerical_attributes[attribute].max()
         for p in range (0, 10):
             percentile = min_value + (max_value - min_value) * p / 10
             next_percentile = min_value + (max_value - min_value) * (p + 1) / 10
             # how many values are less than or equal to the percentile
-            count = df[(df[attribute] >= percentile) & (df[attribute] <= next_percentile)][attribute].count()
+            count = numerical_attributes[(numerical_attributes[attribute] >= percentile) & (numerical_attributes[attribute] <= next_percentile)][attribute].count()
             counts.append(count)
         plt.figure(figsize=(6, 4))
         x = np.arange(0.5, 10.5, 1)
@@ -33,21 +32,19 @@ def show_numerical_attribute_distribution(df: pd.DataFrame) -> None:
         plt.xticks(ticks = range(0, 10), labels = range(0, 100, 10))
         plt.show()
 
-def show_categorical_attribute_histogram(df: pd.DataFrame) -> None:
-    categorical_attributes = df.columns
-    for attribute in categorical_attributes:
+def show_categorical_attribute_histogram(categorical_attributes: pd.DataFrame) -> None:
+    for attribute in categorical_attributes.columns:
         plt.figure(figsize=(6, 4))
-        df[attribute].value_counts().plot.bar()
+        categorical_attributes[attribute].value_counts().plot.bar()
         plt.title(f'Frequency of Values in {attribute}')
         plt.xlabel(attribute)
         plt.ylabel('Frequency')
         plt.xticks(rotation=45, ha='right')
         plt.show()
 
-def show_numerical_corellation(df: pd.DataFrame) -> None:
-    numerical_attributes = df.columns
+def show_numerical_corellation(numerical_attributes: pd.DataFrame, df: pd.DataFrame) -> None:
     correlation_results = pd.DataFrame(columns=['Coefficient', 'P-value'])
-    for attribute in numerical_attributes:
+    for attribute in numerical_attributes.columns:
         coef, p_value = scipy.stats.pointbiserialr(df[attribute], df['Revenue'])
         correlation_results.loc[attribute] = [coef, p_value]
 
@@ -62,11 +59,10 @@ def show_numerical_corellation(df: pd.DataFrame) -> None:
     plt.xticks(rotation=45, ha='right')
     plt.show()
 
-def show_categorical_corellation(df: pd.DataFrame) -> None:
-    categorical_attributes = df.columns
+def show_categorical_corellation(categorical_attributes: pd.DataFrame, df: pd.DataFrame) -> None:
     chi2_results = pd.DataFrame(columns=['Chi2', 'P-value'])
 
-    for attribute in categorical_attributes:
+    for attribute in categorical_attributes.columns:
         contingency_table = pd.crosstab(df[attribute], df['Revenue'])
         chi2, p_value, _, _ = scipy.stats.chi2_contingency(contingency_table)
         chi2_results.loc[attribute] = [chi2, p_value]
