cabfb7d48cb5bb9956ab4806c2cd76fb5865f05e
Foca Bogdan <81586357+Zeyt8@users.noreply.github.com>
2024-01-10 19:37:21 +0200

    Data exploitation for each split

--- graph_utils.py
+++ graph_utils.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy
 
-def show_frequency(df: pd.DataFrame, column_name: str) -> None:
-    value_counts = df[column_name].value_counts()
+def show_frequency(test: pd.Series, column_name: str, index: str) -> None:
+    value_counts = test.value_counts()
     value_counts.plot.bar()
-    plt.title(f'Frequency of Values in {column_name}')
+    plt.title(f'Frequency of Values in {column_name}. Split: {index}')
     plt.xlabel(column_name)
     plt.ylabel('Frequency')
     plt.show()
 
-def show_numerical_attribute_distribution(numerical_attributes: pd.DataFrame) -> None:
+def show_numerical_attribute_distribution(numerical_attributes: pd.Series, index: str) -> None:
     for attribute in numerical_attributes.columns:
         counts = []
         min_value = numerical_attributes[attribute].min()
@@ -26,26 +26,26 @@ def show_numerical_attribute_distribution(numerical_attributes: pd.DataFrame) ->
         x = np.arange(0.5, 10.5, 1)
         y = np.array(counts)
         plt.bar(x, y)
-        plt.title(f'Distribution of {attribute}')
+        plt.title(f'Distribution of {attribute}. Split: {index}')
         plt.xlabel('Percentile')
         plt.ylabel('Frequency')
         plt.xticks(ticks = range(0, 10), labels = range(0, 100, 10))
         plt.show()
 
-def show_categorical_attribute_histogram(categorical_attributes: pd.DataFrame) -> None:
+def show_categorical_attribute_histogram(categorical_attributes: pd.Series, index: str) -> None:
     for attribute in categorical_attributes.columns:
         plt.figure(figsize=(6, 4))
         categorical_attributes[attribute].value_counts().plot.bar()
-        plt.title(f'Frequency of Values in {attribute}')
+        plt.title(f'Frequency of Values in {attribute}. Split: {index}')
         plt.xlabel(attribute)
         plt.ylabel('Frequency')
         plt.xticks(rotation=45, ha='right')
         plt.show()
 
-def show_numerical_corellation(numerical_attributes: pd.DataFrame, df: pd.DataFrame) -> None:
+def show_numerical_corellation(numerical_attributes: pd.Series, test: pd.Series, index: str) -> None:
     correlation_results = pd.DataFrame(columns=['Coefficient', 'P-value'])
     for attribute in numerical_attributes.columns:
-        coef, p_value = scipy.stats.pointbiserialr(df[attribute], df['Revenue'])
+        coef, p_value = scipy.stats.pointbiserialr(numerical_attributes[attribute], test)
         correlation_results.loc[attribute] = [coef, p_value]
 
     print(correlation_results)
@@ -53,17 +53,17 @@ def show_numerical_corellation(numerical_attributes: pd.DataFrame, df: pd.DataFr
     significant_attributes = correlation_results[correlation_results['P-value'] <= 0.05]
     plt.figure(figsize=(10, 4))
     plt.bar(significant_attributes.index, significant_attributes['Coefficient'])
-    plt.title('Point-Biserial Correlation with Revenue')
+    plt.title(f'Point-Biserial Correlation with Revenue. Split: {index}')
     plt.xlabel('Attribute')
     plt.ylabel('Point-Biserial Correlation Coefficient')
     plt.xticks(rotation=45, ha='right')
     plt.show()
 
-def show_categorical_corellation(categorical_attributes: pd.DataFrame, df: pd.DataFrame) -> None:
+def show_categorical_corellation(categorical_attributes: pd.Series, test: pd.Series, index: str) -> None:
     chi2_results = pd.DataFrame(columns=['Chi2', 'P-value'])
 
     for attribute in categorical_attributes.columns:
-        contingency_table = pd.crosstab(df[attribute], df['Revenue'])
+        contingency_table = pd.crosstab(categorical_attributes[attribute], test)
         chi2, p_value, _, _ = scipy.stats.chi2_contingency(contingency_table)
         chi2_results.loc[attribute] = [chi2, p_value]
 
@@ -72,7 +72,7 @@ def show_categorical_corellation(categorical_attributes: pd.DataFrame, df: pd.Da
     significant_attributes = chi2_results[chi2_results['P-value'] <= 0.05]
     plt.figure(figsize=(10, 4))
     plt.bar(significant_attributes.index, significant_attributes['Chi2'])
-    plt.title('Chi-squared Statistic for Categorical Attributes with Revenue')
+    plt.title(f'Chi-squared Statistic for Categorical Attributes with Revenue. Split: {index}')
     plt.xlabel('Attribute')
     plt.ylabel('Chi-squared Statistic')
     plt.xticks(rotation=45, ha='right')
